a581c8e8aaea6d3c449fa4c51c5540d86ecd2024
Afaq Khan <afaqhussain.khan02@gmail.com>
2025-12-08 12:48:04 +0500

    Update depends.py

--- depends.py
+++ depends.py
@@ -14,7 +14,6 @@ async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 10
 async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
     return commons
 
-
 @app.get("/users/")
 async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
 
